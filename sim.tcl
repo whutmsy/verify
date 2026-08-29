b73fe50b01363fa8b0ec02eaec6bc10d58ef66c6
whutmsy <512551180@qq.com>
2026-08-29 15:21:46 +0800

    git配置

--- sim.tcl
+++ sim.tcl
@@ -1,3 +1,7 @@
+git config --global http.proxy http://127.0.0.1:10808
+git config --global https.proxy http://127.0.0.1:10808
+git config --global --unset http.proxy
+git config --global --unset https.proxy
 set UVM_HOME D:/questasim64_10.6c/verilog_src/uvm-1.1d
 set UVM_DPI_HOME D:/questasim64_10.6c/uvm-1.1d/win64
 vlog +incdir+$UVM_HOME/src -L mtiAvm -L mtiOvm -L mtiUvm -L mtiUPF $UVM_HOME/src/uvm_pkg.sv -f filelist.f
